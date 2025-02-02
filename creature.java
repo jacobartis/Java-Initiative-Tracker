75802279bf872e5d374d8dcc3208e07eab07c34a
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-02-02 21:12:14 +0000

    Created basic creature tracker class.

---
+++ creature.java
@@ -0,0 +1,31 @@
+class Creature {
+    String name = "";
+    int initiative = 0;
+    
+    public Creature(){
+        this.name = "Undefined";
+        this.initiative = 0;
+    }
+
+    public Creature(String name,int initiative){
+        this.name = name;
+        this.initiative = initiative;
+    }
+
+    public String get_name(){
+        return this.name;
+    }
+
+    public int get_initiative(){
+        return this.initiative;
+    }
+
+    public void set_name(String name){
+        this.name = name;
+    }
+
+    public void set_inititative(int initiative){
+        this.initiative = initiative;
+    }
+
+}
