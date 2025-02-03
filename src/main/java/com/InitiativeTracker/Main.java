9859c93f3e57584027c8836d8d81efe7d9f09f80
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-02-03 11:04:31 +0000

    Split UI into main and UIController classes. Added fxml layout system.

---
+++ src/main/java/com/InitiativeTracker/Main.java
@@ -0,0 +1,25 @@
+package com.InitiativeTracker;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception{
+
+        Parent root = FXMLLoader.load(getClass().getResource("/com/InitiativeTracker/layout.fxml"));
+
+        Scene scene = new Scene(root,400,300);
+
+        stage.setTitle("Initiative Tracker");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
