b191691136dda179c6ff928f03fca602b883d90f
jacobartis <ZJAC219@live.rhul.ac.uk>
2025-02-03 12:35:25 +0000

    Updated resource load name.

--- src/main/java/com/InitiativeTracker/Main.java
+++ src/main/java/com/InitiativeTracker/Main.java
@@ -4,16 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("/com/InitiativeTracker/layout.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/com/InitiativeTracker/mainMenuLayout.fxml"));
 
         Scene scene = new Scene(root,400,300);
-
         stage.setTitle("Initiative Tracker");
         stage.setScene(scene);
         stage.show();
